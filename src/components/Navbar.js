b775e50628393f4d1ca332a61db2efcee7bfb1c4
shaghayegh Rabiee <rabiee.sh65@gmail.com>
2021-12-29 00:03:19 +0100

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,56 @@
+import React,{useState,useEffect} from 'react';
+import {Link} from 'react-router-dom';
+import './Navbar.css';
+import { Button } from './Button';
+
+
+
+const Navbar = () => {
+
+    const [click,setClick]= useState(false);
+    const [button,setButton]=useState(true);
+
+    const handleIcon=()=>{
+        setClick(!click);
+    }
+    const closeMobileMenu=()=>{
+        setClick({click:false});
+    }
+    const showButton = () => {
+        if (window.innerWidth <= 960) {
+        setButton(false);
+        } else {
+        setButton(true);
+        }
+    };
+    useEffect(() => {
+    showButton();
+    }, []);
+
+      window.addEventListener('resize', showButton);
+    return ( 
+        <>
+            <nav className='navbar'>
+                <div className='navbar-container'>
+                    <Link to="/" className='navbar-logo'>
+                        TRAVEL <i class="fas fa-route"></i>
+                    </Link>
+                    <div className="menu-icon" onClick={handleIcon}>
+                       {click ? <i class="fas fa-times"></i> : <i class="fas fa-bars"></i> } 
+                    </div>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <li className='nav-item'><Link className='nav-links' to="/" onClick={closeMobileMenu}>Home</Link></li>
+                        <li className='nav-item'><Link className='nav-links' to="/services" onClick={closeMobileMenu}>Services</Link></li>
+                        <li className='nav-item'><Link className='nav-links' to="/product" onClick={closeMobileMenu}>Products</Link></li>
+                        <li className='nav-item'><Link className='nav-links-mobile' to="/signup" onClick={closeMobileMenu}>SignUp</Link></li>
+                        
+
+                    </ul>
+                     {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
+                </div>
+            </nav>
+        </>
+     );
+}
+ 
+export default Navbar;
